7e598e9019b6680d07edd3ebd00722103ab20760
zpyuan6 <zpyuan6@163.com>
2023-05-15 19:18:32 +0100

    add wandb key

--- case_study_disease_segmentation/segment_model_training.py
+++ case_study_disease_segmentation/segment_model_training.py
@@ -151,6 +151,7 @@ if __name__ == "__main__":
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    wandb.login(key=os.getenv('WANDB_KEY'))
     wandb.init(
         project="lifelong-hyperspecial",
         config=args
